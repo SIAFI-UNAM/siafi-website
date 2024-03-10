c68021d8fd56411e2ca15235f4893870d63a043b
Christian Leyva <chrisley304@hotmail.com>
2024-03-09 21:54:06 -0600

    SIAFWEB-2: Deleted bold font on address

--- src/components/Landing/ContactUs/index.tsx
+++ src/components/Landing/ContactUs/index.tsx
@@ -6,33 +6,33 @@ import { signArrow } from "@/image-paths";
 export default function ContactUs() {
 	return (
 		<div className={styles.container}>
-<div className={styles.card}>
-	<Image
-		src={signArrow}
-		alt="Sign arrow"
-		className={styles.signArrow}
-	/>
-	<div className="row">
-		<div className="col-12 col-lg-8">
-			<div className={styles.contactus}>
-	<h4>CONTACTANOS</h4>
-				<h2>¿Quieres decirnos o proponernos algo?</h2>
-				<a href="mailto:contacto@siafi-unam.org">
-			contacto@siafi-unam.org
-				</a>
+			<div className={styles.card}>
+				<Image
+					src={signArrow}
+					alt="Sign arrow"
+					className={styles.signArrow}
+				/>
+				<div className="row">
+					<div className="col-12 col-lg-8">
+						<div className={styles.contactus}>
+							<h4>CONTACTANOS</h4>
+							<h2>¿Quieres decirnos o proponernos algo?</h2>
+							<a href="mailto:contacto@siafi-unam.org">
+								contacto@siafi-unam.org
+							</a>
+						</div>
+					</div>
+					<div className={`col-12 col-lg-4 ${styles.direction}`}>
+						<h3>Encuentranos en:</h3>
+						<p>
+							Edificio CIA, 4to piso, conjunto sur, Circuito
+							interior 3000 Col. Universidad Nacional Autónoma
+							de México Coyoacán, C.P. 04510 Ciudad de México,
+							México
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div className={`col-12 col-lg-4 ${styles.direction}`}>
-
-	<p>
-					Edificio CIA, 4to piso, conjunto sur, Circuito
-					interior 3000 Col. Universidad Nacional Autónoma
-					de México Coyoacán, C.P. 04510 Ciudad de México,
-					México
-			</p>
-		</div>
-	</div>
-</div>
-</div>
 	);
 }
